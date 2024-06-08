e5d76e82a079d7c87c7480f23a3ba3870b41b5bd
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-06-09 05:55:05 +0700

    finished

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,34 @@
 function checkPalindrome(text){
     const result = document.querySelector("#container .palindrome #result");
-    const reversed = text.split("").reverse().join("");
-    if(text === reversed){
-        result.innerHTML = `${text} is a palindrome`;
-    }else{
-        result.innerHTML = `${text} is not a palindrome`;
+    result.innerHTML = "";
+
+    let front = "";
+    for (let char of text) {
+        if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9')) {
+            front += char;
+        }
     }
+    const reversed = front.split("").reverse().join("");
+    const message = document.createElement("p");
+
+    if(front.toLowerCase() === reversed.toLowerCase()){
+        message.textContent = `${text} is a palindrome`;
+    }else{
+        message.textContent = `${text} is not a palindrome`;
+    }
+    result.appendChild(message);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const input = document.querySelector("#input");
-    const button = document.querySelector("#button");
+    const input = document.querySelector("#text-input");
+    const button = document.querySelector("#check-btn");
     
     button.addEventListener("click", function() {
         if(input.value.length === 0){
             alert("Please input a value");
         } else {
             checkPalindrome(input.value);
+            input.value = "";
         }
     });
 });
-
